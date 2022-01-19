041b730503314582b80f3b8a637b48df720167d7
Novotný Matyáš <novotny.matyas@ssakhk.cz>
2022-01-19 12:32:12 +0100

    main

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+window.onscroll = function() {myFunction()};
+function myFunction() {
+if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+document.getElementById("home").className = "sticky";
+} else { 
+document.getElementById("home").className = "";
+}
+}
+
+
+
+document.querySelector(".hamburger").addEventListener("click", (e) => {
+e.preventDefault();
+document.querySelector(".navigation").classList.toggle("active");
+})
+
+
+
+wow = new WOW({
+    boxClass: "wow", 
+    animateClass: "animated", 
+    offset: 0, 
+    mobile: true, 
+    live: true, 
+  });
+  wow.init();
